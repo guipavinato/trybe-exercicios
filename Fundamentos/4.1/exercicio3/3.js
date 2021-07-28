701eb25ac26528cdf07190d4d3e76fe1b87a6c49
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-07-28 19:09:45 -0300

    adiciona exercicio 3

---
+++ Fundamentos/4.1/exercicio3/3.js
@@ -0,0 +1,17 @@
+let a = 90;
+let b = 43;
+let c = 89;
+if (a>b){
+    if(a>c){
+        console.log(a);
+    }else{
+        console.log(c);
+    }
+}else{
+    if(b>c){
+        console.log(b);
+    }else{
+        console.log(c);
+    }
+
+}

76d3354d47273269135b75e5808fba22289dc6d4
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-07-29 18:38:51 -0300

    exercicio 4.2 - 3

--- Fundamentos/4.2/script.js
+++ Fundamentos/4.2/script.js
@@ -1,6 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum=0;
+let media=0;
 for(index of numbers){
     sum += index;
 }
-console.log(sum);
+media = sum/numbers.length;
+console.log(media);

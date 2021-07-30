71c03c5aee6dc37321d4989203d927e66bb37439
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-07-30 20:45:53 -0300

    melhorada a formatação nos exercicios

--- Fundamentos/4.3/exercicio1.js
+++ Fundamentos/4.3/exercicio1.js
@@ -1,7 +1,7 @@
 let n = 5;
 let asterisco = "";
 for(let i = 0;i < n;i += 1){
-    asterisco="";
+    asterisco = "";
     for(let x = 0; x <n; x += 1){
         asterisco += "*";
     }
--- Fundamentos/4.3/exercicio3.js
+++ Fundamentos/4.3/exercicio3.js
@@ -2,13 +2,13 @@ let n = 5;
 let asterisco = "";
 let espaco = "";
 for(let i = 1;i <= n;i += 1){
-    asterisco="";
+    asterisco = "";
     espaco = "";
-    for(let x = 0; x <n-i; x += 1){
+    for(let x = 0; x < n-i; x += 1){
         espaco += " ";
     }
-    for (let y = i; y>0; y-=1){
-        asterisco+="*";
+    for (let y = i; y > 0; y -= 1){
+        asterisco += "*";
     }
     console.log(espaco + asterisco);
 }

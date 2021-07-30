00437af2f51e0a7118d3749717cedabcb43e71ff
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-07-30 01:27:50 -0300

    bonus 4.2 - 3

--- Fundamentos/4.2/bonus.js
+++ Fundamentos/4.2/bonus.js
@@ -1,11 +1,13 @@
 let numbers=[5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let mult=[];
 for (let index = 1; index < numbers.length; index += 1) {
-    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-      if (numbers[index] > numbers[secondIndex]) {
-        let position = numbers[index];
-        numbers[index] = numbers[secondIndex];
-        numbers[secondIndex] = position;
-      }
+    for (let secondIndex = index-1; secondIndex < index; secondIndex += 1) {
+        if(secondIndex<(numbers.length-1)){
+            mult.push((numbers[index])*numbers[secondIndex])
+        }
     }
-  }
-  console.log(numbers);
+    if (index == numbers.length-1){
+        mult.push((numbers[index])*2)
+    }
+}
+console.log(mult);

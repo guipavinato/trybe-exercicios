3bf8a2b7a216f3d564a2d8536dcc26e4668de4f4
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-07-29 23:40:58 -0300

    exercicio 4.2 - 7

--- Fundamentos/4.2/script.js
+++ Fundamentos/4.2/script.js
@@ -1,13 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let impar = [];
-for (let index=0; index<numbers.length; index+=1){
-    if (numbers[index]%2!=0){
-        impar.push(numbers[index]);
+let menor = numbers[0];
+for (let index=1; index<numbers.length; index+=1){
+    if (numbers[index]<=menor){
+        menor = numbers[index];
     }
 }
-if (impar.length>0){
-    console.log("Valor(es) ímpar(es):", impar);
-}else{
-    console.log("nenhum valor ímpar encontrado");
-}
-
+console.log("O menor número é:", menor);

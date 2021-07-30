26daec1331fde86a4d34a61c6e405a3f9e6f8656
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-07-29 22:27:09 -0300

    exercicio 4.2 - 5

--- Fundamentos/4.2/script.js
+++ Fundamentos/4.2/script.js
@@ -1,12 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum=0;
-let media=0;
-for(index of numbers){
-    sum += index;
+let maior = 0;
+for (let index=0; index<numbers.length; index+=1){
+    if (numbers[index]>=maior){
+        maior = numbers[index];
+    }
 }
-media = sum/numbers.length;
-if (media>=20){
-    console.log("valor maior que 20");
-}else{
-    console.log("valor menor que 20");
-}
+console.log("O maior número é:", maior);

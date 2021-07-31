43a36ed017db74c6f2e2675d57fa942a12a02222
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-07-30 22:08:07 -0300

    otimização do exercício 4

--- Fundamentos/4.3/exercicio4.js
+++ Fundamentos/4.3/exercicio4.js
@@ -8,12 +8,10 @@ if (n%2 == 0){
     andares = (n/2)+1;
     impar = 1;
 }
-let asterisco = "";
-let espaco = "";
 for(let i = 1;i <= andares;i += 1){
-    asterisco = "";
-    espaco = "";
-    for(let x = 0; x < andares-i; x += 1){
+    let asterisco = "";
+    let espaco = "";
+    for(let x = impar; x < andares-i; x += 1){
         espaco += " ";
     }
     for (let y = i; y > 0; y -= 1){

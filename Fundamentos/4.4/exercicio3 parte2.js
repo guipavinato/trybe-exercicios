44af53fc456f1cf1ea0c89b5a48bf527c7683391
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-08-02 22:43:30 -0300

    adiciona exercício 3 parte 2

---
+++ Fundamentos/4.4/exercicio3 parte2.js
@@ -0,0 +1,13 @@
+let array=[2, 4, 6, 7, 10, 0, -3]
+function maiorValor(array){
+  let posicao=0;
+  for(let i = 0; i<array.length; i+=1){
+    menor=array[i];
+    if (array[i]<=menor){
+      menor=array[i]
+      posicao=i;
+    }
+  }
+  return posicao;
+}
+console.log(maiorValor(array));

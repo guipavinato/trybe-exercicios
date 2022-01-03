2b3a2fe27488e07192e63273e84fa2d5b36b8ed7
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2022-01-02 23:42:14 -0300

    criando funcao Task e array de compromissos

--- Front-end/10.1/my-app/src/App.js
+++ Front-end/10.1/my-app/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import React from 'react';
 
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const compromissos = [
+  'Tocar contrabaixo', 
+  'fazer exercício', 
+  'lavar a louça', 
+  'varrer o quintal', 
+  'lavar o carro', 
+  'passear com o cachorro', 
+  'regar as plantas',
+  'colocar água pássaros'
+]
+
 class App extends React.Component {
   render() {
     return (
       <>
-  
+        {
+          compromissos.map((compromisso) => Task(compromisso))
+        }
       </>
     );
   }

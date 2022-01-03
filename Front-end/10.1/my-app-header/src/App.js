e981cf1f2e0d38e98256a68507f5255bd0b114a6
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2022-01-03 00:20:06 -0300

    criando o map do array de conteudos no content

--- Front-end/10.1/my-app-header/src/App.js
+++ Front-end/10.1/my-app-header/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
 import Header from './Header';
+import Content from './Content';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
+        <Content />
       </>
     );
   };
--- Front-end/10.1/my-app-header/src/Content.jsx
+++ Front-end/10.1/my-app-header/src/Content.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { isElementOfType } from "react-dom/cjs/react-dom-test-utils.production.min";
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+];
 
 class Content extends React.Component {
   render() {
     return (
-      <>
-
-      </>
+      <div className="content">
+        {conteudos.map((conteudo) => (
+          <div key={conteudo.conteudo} className="card">
+            <h3>{`O conteúdo é: ${conteudo.conteudo}`}</h3>
+            <p>{`Status: ${conteudo.status}`}</p>
+            <p>{`Bloco: ${conteudo.bloco}`}</p>
+          </div>  
+        ))}
+      </div>
     );
   };
 };

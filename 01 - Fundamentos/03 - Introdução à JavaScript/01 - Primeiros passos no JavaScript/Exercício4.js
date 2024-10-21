e485ebe9388030b718b56caa5b5ae18d3ec38e6b
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2024-10-21 15:40:14 -0300

    add exerc4-js

---
+++ 01 - Fundamentos/03 - Introdução à JavaScript/01 - Primeiros passos no JavaScript/Exercício4.js
@@ -0,0 +1,10 @@
+let anguloInterno1 = 30;
+let anguloInterno2 = 60;
+let anguloInterno3 = 90;
+let somaDosAngulos = (anguloInterno1 + anguloInterno2 + anguloInterno3);
+
+if (somaDosAngulos == 180 && (anguloInterno1 > 0 && anguloInterno2 >0 && anguloInterno3 > 0)) {
+    console.log(true);
+} else {
+    console.log(false);
+}

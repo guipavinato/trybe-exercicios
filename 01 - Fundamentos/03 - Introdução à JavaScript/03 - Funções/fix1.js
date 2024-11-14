4bf3d722161036d8175621dd392018317a04f67c
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2024-11-14 14:06:38 -0300

    exercicios fixações funcoes

---
+++ 01 - Fundamentos/03 - Introdução à JavaScript/03 - Funções/fix1.js
@@ -0,0 +1,14 @@
+const balance = 100;
+
+function sumBalance(value) { return balance + value }
+
+function subBalance(value) { return balance - value }
+
+function multBalance(value) { return balance * value }
+
+function divBalance(value) { return balance / value }
+
+console.log(sumBalance(10));
+console.log(subBalance(10));
+console.log(multBalance(10));
+console.log(divBalance(10));

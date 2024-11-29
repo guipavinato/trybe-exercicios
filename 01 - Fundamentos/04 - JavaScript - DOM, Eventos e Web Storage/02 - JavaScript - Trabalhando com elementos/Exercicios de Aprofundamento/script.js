cbe16d3cd63446651b511b32c7a2df2a87950053
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2024-11-28 21:27:44 -0300

    add exerc dom dia 2

---
+++ 01 - Fundamentos/04 - JavaScript - DOM, Eventos e Web Storage/02 - JavaScript - Trabalhando com elementos/Exercicios de Aprofundamento/script.js
@@ -0,0 +1,58 @@
+// exercicio 1
+const titulo = document.createElement('h1');
+const bodyPai = document.querySelector('body');
+titulo.innerText = 'TrybeTrip - Agência de Viagens';
+bodyPai.appendChild(titulo);
+
+// exercicio 2
+const principal = document.createElement('main');
+principal.className = 'main-content';
+bodyPai.appendChild(principal);
+
+// exercicio 3
+const center = document.createElement('section');
+center.className = 'center-content';
+principal.appendChild(center)
+
+// exercicio 4
+const paragraph = document.createElement('p');
+paragraph.innerText = 'Texto de teste da criacao do paragrafo'
+center.appendChild(paragraph)
+
+// exercicio 5
+const esquerdo = document.createElement('section');
+esquerdo.className = 'left-content'
+principal.appendChild(esquerdo);
+
+// exercicio 6
+const direito = document.createElement('section');
+direito.className = 'right-content'
+principal.appendChild(direito);
+
+// exercicio 7
+const imagem = document.createElement('img');
+imagem.src = 'https://picsum.photos/200';
+imagem.className = 'small-image';
+esquerdo.appendChild(imagem);
+
+// exercicio 8
+const lista = document.createElement('ul');
+direito.appendChild(lista);
+const listaItens = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez']
+for (let i = 0; i< listaItens.length; i += 1) {
+    const item = document.createElement('li')
+    item.innerText = listaItens[i];
+    lista.appendChild(item);
+}
+
+// exercicio 9
+const headThreeOne = document.createElement('h3');
+headThreeOne.innerText = 'subtitulo - 1';
+const headThreeTwo = document.createElement('h3');
+headThreeTwo.innerText = 'subtitulo - 2';
+const headThreeThree = document.createElement('h3');
+headThreeThree.innerText = 'subtitulo - 3';
+
+principal.appendChild(headThreeOne);
+principal.appendChild(headThreeTwo);
+principal.appendChild(headThreeThree);

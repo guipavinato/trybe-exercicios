bef23d98b9103c892668ef71c0404712a2afdb1b
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2024-12-03 14:26:50 -0300

    add exerc 4 eventsListener

--- 01 - Fundamentos/04 - JavaScript - DOM, Eventos e Web Storage/03 - JavaScript - Eventos/addEventListener/script.js
+++ 01 - Fundamentos/04 - JavaScript - DOM, Eventos e Web Storage/03 - JavaScript - Eventos/addEventListener/script.js
@@ -39,6 +39,12 @@ function alteraTexto(event) {
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
 
+myWebpage.addEventListener("dblclick", redirecionaPagina)
+
+function redirecionaPagina() {
+  window.open('https://www.google.com.br');
+}
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo.
 

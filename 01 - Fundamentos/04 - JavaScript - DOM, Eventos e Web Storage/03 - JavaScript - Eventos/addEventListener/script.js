7646e150d204fdcf92ab8f3c8ecdf37dba219f9a
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2024-12-03 13:52:32 -0300

    add exerc 3 eventsListener

--- 01 - Fundamentos/04 - JavaScript - DOM, Eventos e Web Storage/03 - JavaScript - Eventos/addEventListener/script.js
+++ 01 - Fundamentos/04 - JavaScript - DOM, Eventos e Web Storage/03 - JavaScript - Eventos/addEventListener/script.js
@@ -28,6 +28,13 @@ function addTag(event) {
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech'.
 
+input.addEventListener("keydown", alteraTexto);
+
+function alteraTexto(event) {
+  const classTech = document.querySelector('.tech');
+  classTech.innerText = input.value;
+}
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.

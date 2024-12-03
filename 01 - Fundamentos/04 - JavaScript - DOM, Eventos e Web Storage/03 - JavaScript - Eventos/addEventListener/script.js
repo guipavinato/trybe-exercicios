9b7207e7201c2bef51bd438b51f1f462af02adbf
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2024-12-03 14:43:58 -0300

    add exerc 5 eventsListener

--- 01 - Fundamentos/04 - JavaScript - DOM, Eventos e Web Storage/03 - JavaScript - Eventos/addEventListener/script.js
+++ 01 - Fundamentos/04 - JavaScript - DOM, Eventos e Web Storage/03 - JavaScript - Eventos/addEventListener/script.js
@@ -48,6 +48,17 @@ function redirecionaPagina() {
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo.
 
+myWebpage.addEventListener("mouseover", alteraCorSpot)
+myWebpage.addEventListener("mouseleave", resetaCorSpot)
+
+function alteraCorSpot(event) {
+  event.target.style.color = 'blue';
+}
+
+function resetaCorSpot(event) {
+  event.target.style.color = 'white';
+}
+
 // Segue abaixo um exemplo de uso do event.target.
 
 const resetText = (event) => {
